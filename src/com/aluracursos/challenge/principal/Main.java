91eb0c78ca07cff8ac535dc03e662628ac7ce697
U-Nico-PR <ulisesnicolasp_r_@hotmail.com>
2025-06-01 23:05:19 -0600

    Se hace la Clase Petición con el Request y se hace una pruba con resultado exitoso

--- src/com/aluracursos/challenge/principal/Main.java
+++ src/com/aluracursos/challenge/principal/Main.java
@@ -1,35 +1,18 @@
 package com.aluracursos.challenge.principal;
 
+import com.aluracursos.challenge.servicio.Peticion;
+
 import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public class Main {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args)  {
+        Peticion peticion = new Peticion("USD", "MXN");
 
-        /*
-        Para hacer un request se utiliza una arquitectura Cliente-Servidor
-        Somos el cliente que pide los datos al servidor.
-        El Request pregunta que es lo que queremos
-         */
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://v6.exchangerate-api.com/v6/69ef57f28f49fe4ee5b8578d/latest/USD")) // Aquí va la URL de la API
-                .build();
+        try{
+            peticion.peticionar();
+        }catch (IOException | InterruptedException e){
+            System.out.println("Ocurrio un error: " + e.getMessage());
+        }
 
-        /*
-        Recive la respuesta
-        el metodo "send" puede regresar un error
-        por esta razón se debe hacer un try-catch-finally
-         */
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-
-        /*
-        Imprimimos la respuesta de la petición
-         */
-        System.out.println(response.body());
     }
 }
---
+++ src/com/aluracursos/challenge/servicio/Peticion.java
@@ -0,0 +1,46 @@
+package com.aluracursos.challenge.servicio;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Peticion {
+
+    private String base;
+    private String convertir;
+    private String URL;
+    private String clave = "69ef57f28f49fe4ee5b8578d";
+
+    public Peticion(String base, String convertir){
+        this.base = base;
+        this.convertir = convertir;
+        URL = "https://v6.exchangerate-api.com/v6/"+clave+"/pair/"+base+"/"+convertir+"/";
+    }
+
+    public void peticionar() throws IOException, InterruptedException {
+        /*
+        Para hacer un request se utiliza una arquitectura Cliente-Servidor
+        Somos el cliente que pide los datos al servidor.
+        El Request pregunta que es lo que queremos
+         */
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(URL)) // Aquí va la URL de la API
+                .build();
+
+        /*
+        Recive la respuesta
+        el metodo "send" puede regresar un error
+        por esta razón se debe hacer un try-catch-finally
+         */
+        HttpResponse<String> response = client
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        /*
+        Imprimimos la respuesta de la petición
+         */
+        System.out.println(response.body());
+    }
+}
